656812a81d660167cd3f8aa8097a6db7476f679e
Enrique Rodriguez Miranda <enrique.romir@gmail.com>
2015-02-25 11:41:04 +0000

    Cambio de los comandos del gamepad de Ackerman a Twist

--- summit_robot/summit_controller_dspic/src/summit_controller_dspic.cpp
+++ summit_robot/summit_controller_dspic/src/summit_controller_dspic.cpp
@@ -45,7 +45,7 @@ summit_controller_dspic::summit_controller_dspic( std::string port ) {
         dsPic_data.odometry_yaw = 0.0;
 	pthread_mutex_init(&mutex_odometry, NULL); //Initialization for odometry's mutex
 	iErrorType = DSPIC_ERROR_NONE;
-	active_kinematic_mode_ = DUAL_ACKERMANN_INVERTED; // SINGLE_ACKERMANN;  // Default kinematic mode
+    active_kinematic_mode_ = SINGLE_ACKERMANN; //DUAL_ACKERMANN_INVERTED; // SINGLE_ACKERMANN;  // Default kinematic mode
 
 	// Could be read from parameter yaml file
 	dsPic_data.offset1[DSPIC_FWD_DIRECTION] = 5;
@@ -1591,10 +1591,10 @@ int main(int argc, char** argv)
         
 	tf::TransformBroadcaster tf_broadcaster;
 	// Subcscribing
-    ros::Subscriber cmd_vel_sub = n.subscribe<geometry_msgs::Twist>("/summit_controller_dspic/cmd_vel", 1, &summit_controller_dspic::cmdVelCallback, summit_controller);
+    ros::Subscriber cmd_vel_sub = n.subscribe<geometry_msgs::Twist>("/cmd_vel", 1, &summit_controller_dspic::cmdVelCallback, summit_controller);
 	
 	// Subscribe to command topic
-    //ros::Subscriber cmd_sub_ = n.subscribe<ackermann_msgs::AckermannDriveStamped>("/command", 1, &summit_controller_dspic::commandCallback, summit_controller);
+    //ros::Subscriber cmd_sub_ = n.subscribe<ackermann_msgs::AckermannDriveStamped>("/summit/command", 1, &summit_controller_dspic::commandCallback, summit_controller);
 		
     ros::Subscriber joy_sub = n.subscribe<sensor_msgs::Joy>("/joy", 1, &summit_controller_dspic::joystickCallback, summit_controller);
 
