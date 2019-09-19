9f9e3e2b66b8f73f62df788d49c35e4a706a346f
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-09-19 10:01:14 +0200

    first simple version of telegram bot

---
+++ summit_telegram_bot/src/summit_telegram_bot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    token = open("summit-telegram-token.txt", "r").read().rstrip()
+    print "Using TOKEN: " + token
+    updater = Updater(token)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
